ca65b7a1860cd871e1fc1c2d7781e49735bee6ed
Lee Amado <leeamado22@gmail.com>
2026-05-30 22:59:27 -0300

    Practicas 15-20

---
+++ clase-07/practica-16/js/script.js
@@ -0,0 +1,23 @@
+let nombre = 'Lee ';
+
+let apellido = 'Amado';
+
+let nombreCompleto = 'Cuenta la leyenda que ' + nombre + apellido + ' estudia en la UNA';
+
+let miEdad = 28;
+
+const nombreUna = 'Universidad Nacional de las Artes'; //si modifico la constante más adelante será error
+
+console.log(nombreCompleto + ', tiene ' + miEdad + ' años y el nombre de la universidad es: ' + nombreUna);
+
+nombre = 'Sebastian ';
+
+apellido = 'Lopez';
+
+nombreCompleto = 'Dicen que ' + nombre + apellido + ' hace deporte';
+
+miEdad = 15;
+
+//nombreUna = 'Universidad de Buenos Aires';
+
+console.log(nombreCompleto + ', tiene ' + miEdad + ' años y ahora estudia en la ' + nombreUna);
